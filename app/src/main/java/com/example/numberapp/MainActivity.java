a1865c3f1f79d1718dca048cd859987dbf99b6d7
Gonzalo Gamez <takosaga@gmail.com>
2019-11-18 22:10:15 -0600

    Initial

---
+++ app/src/main/java/com/example/numberapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.numberapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void numButtonPress(View view) {
+        TextView textView = findViewById(R.id.numberView);
+
+        EditText numberEditText = findViewById(R.id.number);
+
+        int numOne = Integer.parseInt(numberEditText.getText().toString());
+
+        textView.setText(Integer.toString(numOne));
+
+    }
+}
